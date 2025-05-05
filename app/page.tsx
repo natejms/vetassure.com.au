22e1047d0061c78fbc8be506118ccd192e3a2f2d
nathanwilson <nathanwil96@gmail.com>
2025-05-05 11:07:20 +1000

    lock site

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import LandingPage2 from "./components/landing-page-2";
 import MainApp from "./components/main-app";
 
-const SITE_OPEN = true;
+const SITE_OPEN = false;
 
 export default function Home() {
   if (!SITE_OPEN) return <LandingPage2 />;
